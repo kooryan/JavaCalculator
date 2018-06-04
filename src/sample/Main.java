ed6545e310bbefbfbda83c3281c142f5117778df
Ryan K <ryank21152@isd273.org>
2018-06-04 17:30:55 -0500

    added icon picture

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application  {
@@ -13,6 +14,8 @@ public class Main extends Application  {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("4function.fxml"));
+        Image applicationIcon = new Image(getClass().getResourceAsStream("calculatoricon2.png"));
+        primaryStage.getIcons().add(applicationIcon);
         primaryStage.setTitle("Java Calculator");
         primaryStage.setScene(new Scene(root, 326, 451));
         primaryStage.show();
